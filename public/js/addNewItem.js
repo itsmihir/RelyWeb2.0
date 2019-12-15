503528dd43a19d34444dc79d9fc59d60e42c8670
itsmihir <inmihir11@gmail.com>
2019-12-15 12:04:49 +0530

    product400

--- public/js/addNewItem.js
+++ public/js/addNewItem.js
@@ -1,34 +1,20 @@
-var firebaseConfig = {
-    apiKey: "AIzaSyCo8HJ00OidtCm5kfDHrBJyDZtXNYR2LUc",
-    authDomain: "rely-fbd57.firebaseapp.com",
-    databaseURL: "https://rely-fbd57.firebaseio.com",
-    projectId: "rely-fbd57",
-    storageBucket: "rely-fbd57.appspot.com",
-    messagingSenderId: "316014605106",
-    appId: "1:316014605106:web:f09786028d1a14e4d65d2b",
-    measurementId: "G-Y3DZ62S9GG"
-};
-// Initialize Firebase
-var app = firebase.initializeApp(firebaseConfig);
-
 var database = firebase.database();
 
-async function saveData(data)
-{
-  var res =  await database.ref('products').push(data, (error)=>
-    {
+async function saveData(data) {
+    var res = await database.ref('Products').push(data, (error) => {
         if (error) {
             console.log('ooops');
-            } else {
+        } else {
             console.log('data saved!');
-            }    
+
+        }
     });
 
+    window.location.href = '../html/index.html';
 }
 
-document.querySelector("#saveNewData").addEventListener("submit", () => {
-    const input = document.querySelector("#inputForm");
-   
+document.querySelector("#saveNewData").addEventListener("click", () => {
+    event.preventDefault();
     var id = document.querySelector("#pid").value;
     var name = document.querySelector("#pname").value;
     var url1 = document.querySelector("#purl").value;
@@ -38,8 +24,13 @@ document.querySelector("#saveNewData").addEventListener("submit", () => {
     var stock = document.querySelector("#pstock").value;
     var price = document.querySelector("#pprice").value;
     var packingType = document.querySelector("#pptype").value;
+
+    if (name == "" || url1 == '' || url2 == '' || Description == '' || tags == '' || stock == '' || price == '' || packingType == '') {
+        alert('Please Fill all the Data');
+        return;
+    }
     var data = {
-        id: id,
+        Id: id,
         Description: Description,
         Image: url1,
         PackagingType: packingType,
@@ -52,3 +43,20 @@ document.querySelector("#saveNewData").addEventListener("submit", () => {
 
     saveData(data);
 });
+
+function fill(id) {
+
+    window.location.href = '../html/addNewItem.html';
+    console.log(data);
+    alert('ads');
+    document.querySelector("#pid").value = data[id].Id;
+    document.querySelector("#pname").value = data[id].ProductName;
+    document.querySelector("#purl").value = data[id].Image;
+    document.querySelector("#purl2").value = data[id].ThumbnailImage;
+    document.querySelector("#description").value = data[id].Description;
+    document.querySelector("#ptags").value = data[id].Tags;
+    document.querySelector("#pstock").value = data[id].Stocks;
+    document.querySelector("#pprice").value = data[id].Price;
+    document.querySelector("#pptype").value = data[id].PackagingType;
+
+}
--- public/js/script.js
+++ public/js/script.js
@@ -1,20 +1,90 @@
-
-  var firebaseConfig = {
-    apiKey: "AIzaSyCo8HJ00OidtCm5kfDHrBJyDZtXNYR2LUc",
-    authDomain: "rely-fbd57.firebaseapp.com",
-    databaseURL: "https://rely-fbd57.firebaseio.com",
-    projectId: "rely-fbd57",
-    storageBucket: "rely-fbd57.appspot.com",
-    messagingSenderId: "316014605106",
-    appId: "1:316014605106:web:f09786028d1a14e4d65d2b",
-    measurementId: "G-Y3DZ62S9GG"
-  };
-  // Initialize Firebase
-  var app = firebase.initializeApp(firebaseConfig);
- // firebase.analytics();
-  console.log(app);
-
-
 var database = firebase.database();
 
+var ref = database.ref('Products');
 
+ref.on('value', gotData, error);
+
+
+function error(error) {
+    console.log(error);
+}
+
+
+
+
+const xaxis = [],
+    yaxis = [];
+
+var data;
+
+function gotData(data) {
+    console.log(data.val());
+    data = data.val();
+    var keys = Object.keys(data);
+    console.log(keys);
+    for (var i = 0; i < keys.length; i++) {
+
+
+        $('#table').append(`<tr>
+    <td>${data[keys[i]].Id}</td>
+    <td>${data[keys[i]].ProductName}</td>
+    <td>${data[keys[i]].Description}</td>
+    <td><a href ="${data[keys[i]].Image}">${data[keys[i]].Image}</a></td>
+    <td><a href ="${data[keys[i]].ThumbnailImage}">${data[keys[i]].ThumbnailImage}</a></td>
+    <td>${data[keys[i]].Tags}</td>
+    <td><strong>${data[keys[i]].Stock}</strong></td>
+    <td><strong>₹${data[keys[i]].Price}</strong></td>
+    <td><button class="btn btn-info" id="${keys[i]}" onclick="editData(this.id)" >Edit</button></td>
+    </tr>
+    `)
+
+        xaxis.push(data[keys[i]].ProductName);
+        yaxis.push(data[keys[i]].Stock);
+    }
+    console.log(xaxis);
+    console.log(yaxis);
+
+    var ctx = document.getElementById('myChart').getContext('2d');
+    var myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: xaxis,
+            datasets: [{
+                label: 'Stock',
+                data: yaxis,
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
+}
+
+
+function editData(Id) {
+    console.log(Id);
+    fill(Id);
+}
